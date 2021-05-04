3bba3b9476f82d331cd964c46960228d74b1cc7d
Sarra <sarra.ismail@eniso.u-sousse.tn>
2021-05-04 04:19:14 +0100

    Increment

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,23 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      //Add setState this to could update the counter
+      _counter += 2;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,15 +29,25 @@ class MyApp extends StatelessWidget {
           title: Text("Sarra ISMAIL"),
         ),
         body: Container(
+            color: Colors.lightBlue,
             alignment: Alignment.center,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text("Body Title"),
-                Text("Body title"),
-                Text("Body Title"),
+              children: [
+                Text("you have pushed the button this many times: "),
+                Text(
+                  '$_counter',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .headline3,
+                ),
               ],
             )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
