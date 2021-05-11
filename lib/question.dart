f2a50f898e298fd3f6220d9aa75d4d8df9e0d785
Sarra <sarra.ismail@eniso.u-sousse.tn>
2021-05-11 05:23:52 +0100

    Quiz_App1.V1

--- lib/question.dart
+++ lib/question.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 class Question extends StatelessWidget {
+
+  final String questionText;
+
+  Question(this.questionText) ;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      margin : EdgeInsets.all(20.0),
+      child: Text(questionText,
+        style:TextStyle(fontSize: 25),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
