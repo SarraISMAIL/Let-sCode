75942f8e9d19fa1ed6479a3ed2d4165fc3fb0458
Sarra <sarra.ismail@eniso.u-sousse.tn>
2021-05-11 05:23:27 +0100

    Quiz_App1.V1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
+main() => runApp(MyApp());
 
-main()=> runApp(MyApp());
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
+  final _question = [
+    "What\'s Your favorite Color ?",
+    "What\'s Your favorite Animal ?"
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Quiz App"),
+      ),
+      body: Container(
+        child : Column(
+          children : <Widget>[
+            Question(_question[1]),
+            RaisedButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              child  : Text("Answer 1"),
+                onPressed:(){},
+
+            )
+
+          ],
+        ),
+
+      ),
+    ));
   }
 }
